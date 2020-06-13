3a4d798f2cdb391bd5adeccecb8f10ea75aa6f22
guilhermeNatan <guilherme.natan.barbosa@gmail.com>
2020-06-13 18:37:56 -0300

    adicionando locales

--- initproject.sh
+++ initproject.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-react-native init $1 &&  cd $1 && yarn add lodash  react-native-gesture-handler react-native-vector-icons @react-navigation/drawer  @react-navigation/native   react-navigation-drawer react-navigation-transitions react-redux redux redux-thunk  yup formik react-native-dropdownalert react-native-paper  @react-native-community/masked-view  react-native-safe-area-context react-native-screens  firebase react-native-reanimated  @react-navigation/native @react-navigation/material-bottom-tabs  @react-navigation/stack && cp -rb ../templates/* . &&  
+react-native init $1 &&  cd $1 && yarn add lodash  react-native-gesture-handler react-native-vector-icons @react-navigation/drawer  @react-navigation/native   react-navigation-drawer react-navigation-transitions react-redux redux redux-thunk  yup formik react-native-dropdownalert react-native-paper  @react-native-community/masked-view  react-native-safe-area-context react-native-screens  firebase react-native-reanimated  @react-navigation/native @react-navigation/material-bottom-tabs  @react-navigation/stack i18n-js && cp -rb ../templates/* . &&  
 
 ## app/build.gradle
 ## add this line
--- templates/src/screens/Home/HomeScreen.js
+++ templates/src/screens/Home/HomeScreen.js
@@ -8,12 +8,13 @@
 
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
+import { translate } from '../../locales'; 
 
 class HomeScreen extends Component {
   render() {
     return (
       <View>
-         <Text>{`HOME`}</Text>
+         <Text>{translate('hello')}</Text>
       </View>
     );
   }
